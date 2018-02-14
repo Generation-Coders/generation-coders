50ff6cdf1ebe428f13bdbe676077e166bf91008c
irisval <rissy.val19@gmail.com>
2018-02-13 22:22:41 -0500

    Added routing for reflections.

--- app/views.py
+++ app/views.py
@@ -13,10 +13,14 @@ def about():
 def showcase():
 	return render_template('showcase.html')
 
+@app.route('/showcase/<place>')
+def reflections(place):
+	return render_template('reflections/{0}.html'.format(place))
+
 @app.route('/upcoming')
 def upcoming():
 	return render_template('upcoming.html')
 
-# @app.route('/resources')
-# def index():
-# 	return render_template('resources.html')
+@app.route('/resources')
+def resources():
+	return render_template('resources.html')
