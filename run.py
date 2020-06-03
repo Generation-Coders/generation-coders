a30527bf100bc553715bd3e761d7ce661aebe1e3
3michelin <remingtonskim@gmail.com>
2020-06-03 13:22:45 -0400

    changed host

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 from app import app
 
 if __name__ == '__main__':
-	app.run(host='localhost', port=int(os.environ.get('PORT')), debug=os.environ.get('doDEBUG'))
+	app.run(host='0.0.0.0', port=int(os.environ.get('PORT')), debug=os.environ.get('doDEBUG'))
 
 
